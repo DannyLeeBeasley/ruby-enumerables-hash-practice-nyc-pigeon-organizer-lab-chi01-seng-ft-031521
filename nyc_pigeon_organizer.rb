2ca06fb41ffb64babfdf7305402661317e486365
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-08 17:26:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
   final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
@@ -13,4 +14,16 @@ def nyc_pigeon_organizer(data)
   end
   end
  end
+=======
+  data.each_with_object({}) do |(key, value), final_array|
+    #binding.pry 
+    value.each do |inner_key, name|
+      names.each do |name|
+    binding.pry
+  end
+  end
+ final_array
+ end
+ binding.pry
+>>>>>>> b9fdd56c2393909e5a4a4db9b48e27392bf86cd7
 end
